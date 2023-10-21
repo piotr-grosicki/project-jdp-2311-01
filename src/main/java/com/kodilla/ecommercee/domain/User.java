5344e567eaf5aedcd54544a4a937b4eede391b0e
GP <gp@GP.local>
2023-10-21 20:23:10 +0200

    implementacja Encji User Update - Task JDP231101-15

--- src/main/java/com/kodilla/ecommercee/domain/User.java
+++ src/main/java/com/kodilla/ecommercee/domain/User.java
@@ -2,13 +2,49 @@ package com.kodilla.ecommercee.domain;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.ArrayList;
+import java.util.List;
 
-@Entity
 @Data
+@Entity(name = "users")
 public class User {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "userId")
     private Long id;
 
+    @NotNull
+    @Column(name = "username", unique = true)
+    private String username;
+
+    @NotNull
+    @Column(name = "password")
+    private String password;
+
+    @Column(name = "isBlocked")
+    private boolean isBlocked;
+
+    @Column(name = "token", unique = true)
+    private Long token;
+
+    // Relacje do koszyka Cart
+    @OneToMany(
+            targetEntity = Cart.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Cart> cartList = new ArrayList<>();
+
+    // Relacje do zamówień Order
+    @OneToMany(
+            targetEntity = Order.class,
+            mappedBy = "user",
+            cascade = CascadeType.ALL,
+            fetch = FetchType.LAZY
+    )
+    private List<Order> orderList = new ArrayList<>();
 }
