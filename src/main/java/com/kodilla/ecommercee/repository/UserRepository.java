c9458b8c56459ba0072ce03f611ed910b29d6b5d
RafalNicki <Nicol79@op.pl>
2023-10-23 15:11:48 +0200

    create new repository User

--- src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
+++ src/main/java/com/kodilla/ecommercee/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.kodilla.ecommercee.repository;
 import com.kodilla.ecommercee.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository <User,Long> {
+public interface UserRepository extends JpaRepository <User,Long>  {
 }
