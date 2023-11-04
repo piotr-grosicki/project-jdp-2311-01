4a6d9fb862ee8b0a7b755e2acea97ef44e655129
RafalNicki <Nicol79@op.pl>
2023-11-04 14:02:11 +0100

    JDP231101-19

--- src/test/java/com/kodilla/ecommercee/domain/CartCRUDTest.java
+++ src/test/java/com/kodilla/ecommercee/domain/CartCRUDTest.java
@@ -10,7 +10,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
-
 import javax.transaction.Transactional;
 import java.time.LocalDate;
 import java.util.ArrayList;
