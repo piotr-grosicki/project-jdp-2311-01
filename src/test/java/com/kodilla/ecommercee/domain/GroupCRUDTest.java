18999e78db3b34731053bfbdc4cda8f627c502e7
Natalia Wszelaki <natalia.slocinska@gmail.com>
2023-11-13 12:17:06 +0100

    JDP231101-22 GroupController

--- src/test/java/com/kodilla/ecommercee/domain/GroupCRUDTest.java
+++ src/test/java/com/kodilla/ecommercee/domain/GroupCRUDTest.java
@@ -1,11 +1,17 @@
 package com.kodilla.ecommercee.domain;
 
 import com.kodilla.ecommercee.repository.GroupRepository;
+import com.kodilla.ecommercee.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
+
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 public class GroupCRUDTest {
@@ -13,13 +19,23 @@ public class GroupCRUDTest {
     @Autowired
     private GroupRepository groupRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
+    private Group group;
+
+    @BeforeEach
+    public void setUp() {
+        group = new Group();
+        group.setGroupId(1L);
+        group.setName("electronics");
+        group.setDescription("some equipment");
+
+    }
+
     @Test
     @DirtiesContext
     public void testCreateGroup() {
-        //Given
-        Group group = new Group();
-        group.setName("electronics");
-        group.setDescription("some equipment");
 
         //When
         groupRepository.save(group);
@@ -34,10 +50,6 @@ public class GroupCRUDTest {
     @Test
     @DirtiesContext
     public void testFindGroupById() {
-        //Given
-        Group group = new Group();
-        group.setName("electronics");
-        group.setDescription("some equipment");
 
         //When
         groupRepository.save(group);
@@ -46,15 +58,33 @@ public class GroupCRUDTest {
         //Then
         assertNotNull(foundGroup);
         assertEquals(group.getGroupId(),foundGroup.getGroupId());
+        assertEquals(group.getGroupId(), foundGroup.getGroupId());
+        assertEquals(group.getName(), foundGroup.getName());
+        assertEquals(group.getDescription(), foundGroup.getDescription());
+    }
+
+    @Test
+    @DirtiesContext
+    public void testFindAllGroups() {
+
+        //Given
+        Group group2 = new Group();
+        group2.setGroupId(2L);
+        group2.setName("electric stuff");
+        group2.setDescription("jigsaw");
+
+        //When
+        groupRepository.save(group);
+        groupRepository.save(group2);
+        List<Group> groupList = groupRepository.findAll();
+
+        //Then
+        assertThat(groupList.size()).isEqualTo(2);
     }
 
     @Test
     @DirtiesContext
     public void testUpdateGroup() {
-        //Given
-        Group group = new Group();
-        group.setName("electronics");
-        group.setDescription("some equipment");
 
         //When
         Group group1 = groupRepository.save(group);
@@ -72,10 +102,6 @@ public class GroupCRUDTest {
     @Test
     @DirtiesContext
     public void testDeleteGroup() {
-        //Given
-        Group group = new Group();
-        group.setName("electronics");
-        group.setDescription("some equipment");
 
         //When
         Group groupAdd = groupRepository.save(group);
@@ -86,4 +112,53 @@ public class GroupCRUDTest {
         assertNull(groupDelete);
     }
 
+    @Test
+    @DirtiesContext
+    public void testGroupProductRelation() {
+
+        //Given
+        Product product = new Product();
+        product.setProductId(100L);
+        product.setNameProduct("Circular Saw");
+        Product product2 = new Product();
+        product2.setProductId(101L);
+        product2.setNameProduct("Electric screwdriver");
+        productRepository.save(product);
+        productRepository.save(product2);
+
+        //When
+        group.getProductList().add(product);
+        group.getProductList().add(product2);
+        groupRepository.save(group);
+        product.setGroup(group);
+        product2.setGroup(group);
+        Group productGroup = product.getGroup();
+
+        //Then
+        assertEquals(2, group.getProductList().size());
+        assertEquals(group.getGroupId(), productGroup.getGroupId());
+        assertEquals(product.getNameProduct(), productGroup.getProductList().get(0).getNameProduct());
+        assertEquals(product.getProductId(), productGroup.getProductList().get(0).getProductId());
+        assertEquals(product.getDescriptionProduct(), productGroup.getProductList().get(0).getDescriptionProduct());
+        assertEquals(product.getPrice(), productGroup.getProductList().get(0).getPrice());
+        assertEquals(product.getGroup(), productGroup.getProductList().get(0).getGroup());
+        assertEquals(product2.getNameProduct(), productGroup.getProductList().get(1).getNameProduct());
+        assertEquals(product2.getProductId(), productGroup.getProductList().get(1).getProductId());
+        assertEquals(product.getDescriptionProduct(), productGroup.getProductList().get(1).getDescriptionProduct());
+        assertEquals(product.getPrice(), productGroup.getProductList().get(1).getPrice());
+        assertEquals(product.getGroup(), productGroup.getProductList().get(1).getGroup());
+    }
+
+    @Test
+    @DirtiesContext
+    public void testExistByName() {
+
+        //When
+        boolean isExisting = groupRepository.existsByName(group.getName());
+        boolean isExisting2 = groupRepository.existsByName("stuff");
+
+        //Then
+        assertTrue(isExisting);
+        assertFalse(isExisting2);
+    }
 }
